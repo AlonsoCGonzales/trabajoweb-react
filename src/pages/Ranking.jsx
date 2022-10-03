0f3df62193aa3001caeea5c57155f6ed1be2ff67
AlonsoCaceres <alonzotoricaceres@gmail.com>
2022-10-02 22:41:15 -0500

    React

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -59,7 +59,7 @@ const Ranking = () => {
     <div class="container text-center">
       <div class="row">
         <div class="col">
-          <h5>Tarjeta De Video Asus Tuf Gaming Oc gtx1650 4gb Gddr6</h5>
+          <h5>1.- Tarjeta De Video Asus Tuf Gaming Oc gtx1650 4gb Gddr6</h5>
           <div class="progress">
             <div class="progress-bar" role="progressbar" aria-label="Basic example" 
               style={{width: "98%"}} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -69,7 +69,7 @@ const Ranking = () => {
             </div>
           </div>
           <div class="col">
-            <h5>Core i7-11900k</h5>
+            <h5>2.- Core i7-11900k</h5>
             <div class="progress">
               <div class="progress-bar bg-success" role="progressbar" aria-label="Basic example"
                 style={{ width: "91%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -82,7 +82,7 @@ const Ranking = () => {
       </div>
       <div class="row">
         <div class="col">
-          <h5>Gabinete Pc Gamer Cooler Master H500 Argb Templado Midtower</h5>
+          <h5>3.- Gabinete Pc Gamer Cooler Master H500 Argb Templado Midtower</h5>
           <div class="progress">
             <div class="progress-bar bg-warning" role="progressbar" aria-label="Basic example"
               style={{ width: "82%" }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
@@ -93,7 +93,7 @@ const Ranking = () => {
             </div>
           </div>
         <div class="col">
-          <h5>Ssd Sata A400 480gb</h5>
+          <h5>4.- Ssd Sata A400 480gb</h5>
             <div class="progress">
               <div class="progress-bar" role="progressbar" aria-label="Basic example" style={{ width: "69%" }}
               aria-valuenow="25" aria-valuemin="0"aria-valuemax="100"></div>
@@ -105,30 +105,26 @@ const Ranking = () => {
         </div>
         <div class="row">
           <div class="col">
-            <h5>Memoria Ram 8gb 2600mhz Laptop Kingston</h5>
+            <h5>5.- Memoria Ram 8gb 2600mhz Laptop Kingston</h5>
             <div class="progress">
               <div class="progress-bar bg-info" role="progressbar" aria-label="Basic example" style={{ width: "66%" }}
-              aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">
+              aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
             <div class="card" style={{width: "18rem",height: "10rem",marginBottom: "20px",marginLeft: "60px",}}>
-            <img src="/img/ram8.png" class="card-img-top" alt="Core"></img>
+              <img src="/img/ram8.png" class="card-img-top" alt="Core"></img>
+            </div>
           </div>
           <div class="col">
-          <h5>Intel (59%) vs AMD (41%)</h5>
+            <h5>Intel (59%) vs AMD (41%)</h5>
             <div class="progress">
             <div class="progress-bar" role="progressbar" style={{ width: "59%" }} aria-valuenow="15"
-            aria-valuemin="0" aria-valuemax="100">
-          <div class="progress-bar bg-danger" role="progressbar" style={{ width: "41%" }} aria-valuenow="30"
-          aria-valuemin="0" aria-valuemax="100">
-          </div>
+            aria-valuemin="0" aria-valuemax="100"></div>
+            <div class="progress-bar bg-danger" role="progressbar" style={{ width: "41%" }} aria-valuenow="30"
+            aria-valuemin="0" aria-valuemax="100"></div>
           </div>
         </div>
-
       </div>
-      </div>
-    </div>
-    </div>
-    </div>
+  </div>
 }
 
 export default Ranking;
