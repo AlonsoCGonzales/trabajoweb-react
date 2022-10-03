0ec2feebaf2d4a3f0165587e4884899a6ea47c7a
AlonsoCaceres <alonzotoricaceres@gmail.com>
2022-10-02 22:13:40 -0500

    react

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Requets from './pages/Requets';
+import Gamming from './pages/Gamming';
+import Busqueda from './pages/Busqueda';
+import Ofimatica from './pages/Ofimatica';
+import Programing from './pages/Programing';
+import Ranking from './pages/Ranking';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter><Routes>
+      <Route path='/' element={<Busqueda />}/>
+      <Route path='/Gamming' element={<Gamming />}/>
+      <Route path='/Ofimatica' element={<Ofimatica />}/>
+      <Route path='/Programing' element={<Programing />}/>
+      <Route path='/Requets' element={<Requets />}/>
+      <Route path='/Ranking' element={<Ranking />}/>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
